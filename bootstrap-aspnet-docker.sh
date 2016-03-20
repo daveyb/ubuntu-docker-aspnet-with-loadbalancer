60f3c7be60ef5e32d2c2b53443aa8f5bb0c09186
David Benedic <davidbenedic@gmail.com>
2016-03-19 22:52:30 -0400

    Add aspnet docker bootstrap script

--- bootstrap-aspnet-docker.sh
+++ bootstrap-aspnet-docker.sh
@@ -11,7 +11,7 @@ apt-get -y install Git
 
 ## Build and run ASP.NET Core HelloWeb Docker container
 git clone https://github.com/aspnet/Home.git aspnet-Home
-cd aspnet-Home/samples/latest/HellowWeb
+cd aspnet-Home/samples/1.0.0-rc1-final/HellowWeb
 
 docker build -t helloweb .
 
