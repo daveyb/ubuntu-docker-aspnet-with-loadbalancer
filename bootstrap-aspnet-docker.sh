0c68fa6d53a518a601e0983ca99d61fbb69b6eae
--global <--global>
2016-08-08 14:14:59 -0400

    add docker container to systemctl

--- bootstrap-aspnet-docker.sh
+++ bootstrap-aspnet-docker.sh
@@ -9,10 +9,31 @@ curl -sSL https://get.docker.com/ | sh
 ## Install Git
 apt-get -y install Git
 
-## Build and run ASP.NET Core HelloWeb Docker container
+## Build ASP.NET Core HelloWeb Docker container
 git clone https://github.com/aspnet/Home.git /usr/local/src/aspnet-Home
 cd /usr/local/src/aspnet-Home/samples/1.0.0-rc1-final/HelloWeb
 
 docker build -t helloweb .
+
+# Configure Helloweb to startup automatically, start it.
+touch /etc/systemd/system/docker-helloweb.service
+cat << EOF > /etc/systemd/system/docker-aspnet_core.service
+[Unit]
+Description=ASPNET MVC Core container
+Requires=docker.service
+After=docker.service
+
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker run -t -d -p 80:5004 helloweb
+ExecStop=/usr/bin/docker stop -t 5 helloweb
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl daemon-reload
+systemctl start docker-helloweb.service
+systemctl enable docker-helloweb.service
 
 docker run -t -d -p 80:5004 helloweb
